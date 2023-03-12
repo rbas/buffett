01531fec077bbcc0bf3909306282d71484ca6d59
Martin Voldřich <rbas.cz@gmail.com>
2023-03-12 21:08:00 +0100

    Sqlite configuration added

--- src/config.rs
+++ src/config.rs
@@ -4,6 +4,7 @@ use std::fs;
 #[derive(Deserialize, Debug)]
 pub struct Config {
     pub pushover: PushOverConf,
+    pub db: DbConf,
 }
 #[derive(Deserialize, Debug)]
 pub struct PushOverConf {
@@ -12,9 +13,14 @@ pub struct PushOverConf {
     pub delivery_group_token: String,
 }
 
+#[derive(Deserialize, Debug)]
+pub struct DbConf {
+    pub path: String,
+}
+
 pub fn load_config(file_path: &str) -> Config {
     let config_data =
-        fs::read_to_string(file_path).expect("Should have been able to read the file");
+        fs::read_to_string(file_path).expect(format!("Cannot read file `{}`. Please create configuration file from template `conf_template.toml`.", file_path).as_str());
     let config: Config = toml::from_str(&config_data).unwrap();
 
     config
